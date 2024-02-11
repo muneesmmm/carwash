af37ce307f67eee223e67fe808990b1644c5f834
Munees <muneesmmm@gmail.com>
2024-02-11 09:32:34 +0530

    updated

--- modals/washHistory.js
+++ modals/washHistory.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
-const moment = require('moment-timezone');
+
 const washSchema = new mongoose.Schema({
   vehicle: { type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle' },
   staff: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   washDate:{
-    type: String,
+    type: Date,
+    default: Date.now,
   },
   washType: {
     type: String,
     enum: ['Wash', 'Interior'], // Add other package options as needed
 },
 });
-// Static method to set the wash date to the current time in IST
-
 module.exports = mongoose.model('WashHistory', washSchema);
