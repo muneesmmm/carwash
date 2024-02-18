2a9ee05e8478039ec42176ae5f6e11876a5c464b
Munees <muneesmmm@gmail.com>
2024-02-18 15:13:43 +0530

    updated

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ const port = 8080;
 
 app.use(
   cors({
-    origin: "https://muneesmmm.github.io/", // Replace with your frontend's origin URL
+    origin: "https://muneesmmm.github.io", // Replace with your frontend's origin URL
     allowedHeaders: ["Content-Type", "Authorization"], // Add any additional headers you want to allow
   })
 );
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://muneesmmm.github.io/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://muneesmmm.github.io');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
