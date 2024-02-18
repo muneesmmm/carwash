f1a0f692f968f19caf6aab727981241fee484999
Munees <muneesmmm@gmail.com>
2024-02-18 15:11:22 +0530

    updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const uploadHelper = require("./helpers/s3Upload");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(
   cors({
