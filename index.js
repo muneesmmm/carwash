02bda6d0a0d21f997753406c23841d881b8e7a07
Munees <muneesmmm@gmail.com>
2024-03-02 23:22:27 +0530

    updated

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ const port = 8080;
 
 app.use(
   cors({
-    origin: "http://admin.wecarecarwash.in", // Replace with your frontend's origin URL
+    origin: "https://admin.wecarecarwash.in", // Replace with your frontend's origin URL
     allowedHeaders: ["Content-Type", "Authorization"], // Add any additional headers you want to allow
   })
 );
 app.use((req, res, next) => {
-res.setHeader('Access-Control-Allow-Origin', 'http://admin.wecarecarwash.in');
+res.setHeader('Access-Control-Allow-Origin', 'https://admin.wecarecarwash.in');
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 res.setHeader('Access-Control-Allow-Credentials', true);
